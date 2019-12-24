94775731a1da63fa87678f7237430c9d1ca1b24c
hoangtuan <tuanha@viralsoft.vn>
2019-12-24 08:57:09 +0700

    update route

--- src/authRoute.php
+++ src/authRoute.php
@@ -3,9 +3,9 @@
 Route::group(['middleware' => 'web'],  function () {
     Route::get('auth/callback/', '\TuanHA\AuthApiGateway\AuthDemoController@callback' )->name('login.callback');
 
-    Route::get('/login', '\TuanHA\AuthApiGateway\AuthDemoController@login' )->name('auth.login')->middleware('guest');
+    Route::get('/login', '\TuanHA\AuthApiGateway\AuthDemoController@login' )->name('login')->middleware('guest');
 
-    Route::post('/logout', '\TuanHA\AuthApiGateway\AuthDemoController@logout' )->name('auth.login')->middleware('auth');
+    Route::post('/logout', '\TuanHA\AuthApiGateway\AuthDemoController@logout' )->name('logout')->middleware('auth');
 
     Route::fallback(function () {
         return redirect(url('/login'));
